2752dd262b1f2afb5598e94c7d3f286e876314b8
Elias K <elias.kitano@gmail.com>
2026-07-24 18:21:26 +0200

    release: v07.24.06

--- httpaceproxycpp/include/httpaceproxycpp/config.hpp
+++ httpaceproxycpp/include/httpaceproxycpp/config.hpp
@@ -7,7 +7,7 @@
 
 namespace httpace {
 
-inline constexpr const char* kAppVersion = "v07.24.04";
+inline constexpr const char* kAppVersion = "v07.24.05";
 
 struct Config {
     std::string ace_host = "127.0.0.1";
